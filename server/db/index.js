807c658df93247dbd24755f754245071c02b431f
Happy Situ <happy.situ@clutchanalytics.com>
2018-06-07 23:59:21 -0500

    add all

--- server/db/index.js
+++ server/db/index.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: process.env.MYSQL_HOST || 'localhost',
   user: process.env.MYSQL_USER || "root",
-  password: process.env.MYSQL_PASSWORD || "",
+  password: process.env.MYSQL_PASSWORD || "aznxdog94",
   database: "ablebox"
 });
 
