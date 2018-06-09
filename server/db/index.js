50ceec55edb3e36f6821001664a15e05b67b9257
Adelle Housker <housker@gmail.com>
2018-06-09 09:12:20 -0500

    configured for local development

--- server/db/index.js
+++ server/db/index.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: process.env.MYSQL_HOST || 'localhost',
   user: process.env.MYSQL_USER || "root",
-  password: process.env.MYSQL_PASSWORD || "aznxdog94",
+  password: process.env.MYSQL_PASSWORD || "",
   database: "ablebox"
 });
 
