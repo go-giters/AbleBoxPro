420b4a82ae53b2fdef4b2b35eea5fd09bb1ebea2
Adelle Housker <housker@gmail.com>
2018-06-18 23:11:13 -0500

    (chore) added ipfs instructions on login screen

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Row, Col, Button, Form, FormGroup, FormControl, Label, Input } from 'reactstrap';
 import { withRouter } from 'react-router-dom';
 import $ from 'jquery';
+import ipfsAPI from 'ipfs-api';
 
 class Login extends React.Component {
   constructor(props) {
@@ -10,6 +11,9 @@ class Login extends React.Component {
       email: '',
       password: ''
     };
+
+    this.ipfsApi = ipfsAPI('localhost', '5001');
+
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -53,31 +57,37 @@ console.log(parsedUrl.searchParams); // 123
   render() {
     return (
       <div color="light">
-        <div>
-        <br/>
-          <h2 color="light" className="text-center font-weight-bold" id="signInTitle"> Sign In </h2>
-        </div>
-        <br/>
-          <Form onSubmit={this.handleSubmit}>
-            <Row>
-              <Col xs={{ size: 8, offset: 2 }} sm={{ size: 6, offset: 3 }} md={{ size: 6}} lg={{ size: 6}}>
-                <FormGroup>
-                  <Label for="email">Email Address</Label>
-                  <Input type="text" name="email" placeholder="email address" onChange={this.handleChange}></Input>
-                </FormGroup>
-              </Col>
-            </Row>
-            <Row>
-              <Col xs={{ size: 8, offset: 2 }} sm={{ size: 6, offset: 3 }} md={{ size: 6}} lg={{ size: 6}}>
-                <FormGroup>
-                  <Label for="password">Password</Label>
-                  <Input type="password" name="password" placeholder="password" onChange={this.handleChange}></Input>
-                </FormGroup>
-              </Col>
-            </Row>
-            <br/>
-            <Button className="d-block mx-auto btn-outline-primary" type="submit">Login</Button>
-          </Form>
+          <div>
+          <br/>
+            <h2 color="light" className="text-center font-weight-bold" id="signInTitle"> Sign In </h2>
+          </div>
+          <br/>
+            <Form onSubmit={this.handleSubmit}>
+              <Row>
+                <Col xs={{ size: 8, offset: 2 }} sm={{ size: 6, offset: 3 }} md={{ size: 6}} lg={{ size: 6}}>
+                  <FormGroup>
+                    <Label for="email">Email Address</Label>
+                    <Input type="text" name="email" placeholder="email address" onChange={this.handleChange}></Input>
+                  </FormGroup>
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={{ size: 8, offset: 2 }} sm={{ size: 6, offset: 3 }} md={{ size: 6}} lg={{ size: 6}}>
+                  <FormGroup>
+                    <Label for="password">Password</Label>
+                    <Input type="password" name="password" placeholder="password" onChange={this.handleChange}></Input>
+                  </FormGroup>
+                </Col>
+              </Row>
+              <br/>
+              <Button className="d-block mx-auto btn-outline-primary" type="submit">Login</Button>
+            </Form>
+          { !this.ipfsApi.id() && <footer className="footer">
+          <p className="text-muted">This app utilizes block-chain decentralized file storage. Please <a href="https://ipfs.io/docs/install/">download ipfs</a> and configure with the following command-line heades in order to enable this feature</p>
+          <div className="text-muted">ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin "[\"http://example.com\"]"</div>
+          <div className="text-muted">ipfs config --json API.HTTPHeaders.Access-Control-Allow-Credentials "[\"true\"]"</div>
+          <div className="text-muted">ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods "[\"PUT\", \"POST\", \"GET\"]"</div>
+          </footer> }
       </div>
     )
   }
